27092cdd07cb3c2fc3afdfa0c4b07cc858d6338a
Alice Nascimento <145519081+alicevn@users.noreply.github.com>
2024-12-02 22:59:05 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// Botão de cancelar
+document.getElementById('cancel-button').addEventListener('click', () => {
+    alert('Alterações canceladas!');
+    location.reload(); // Recarrega a página
+});
+
+// Botão de salvar
+document.getElementById('save-button').addEventListener('click', () => {
+    alert('Alterações salvas com sucesso!');
+});
